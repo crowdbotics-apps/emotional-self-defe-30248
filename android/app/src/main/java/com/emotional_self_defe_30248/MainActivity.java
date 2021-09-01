cd29b56b679b553eb52a0f535dc26345f46b5f0c
Crowdbotics <team@crowdbotics.com>
2021-09-01 13:39:46 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/emotional_self_defe_30248/MainActivity.java
@@ -0,0 +1,15 @@
+package com.emotional_self_defe_30248;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "emotional_self_defe_30248";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "emotional_self_defe_30248Identifier",
+  url: "https://emotional_self_defe_30248Identifier.botics.co",
+  api: "https://emotional_self_defe_30248Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
